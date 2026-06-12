b1eedae1d516871db7947ca98dccc6e1ce9ff908
Oscar Swan <oscarswan25@gmail.com>
2026-06-12 18:44:34 +0100

    Add data loader

---
+++ src/data_loader.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import os
+
+
+def download_stock_data(ticker, start_date, end_date):
+    #Downloads a specific companies stock data and saves it as a CSV
+    print(f"Downloading {ticker} data...")
+    df = yf.download(ticker, start=start_date, end=end_date)
+    os.makedirs("../data", exist_ok=True)
+    filepath = f"../data/{ticker}_{start_date}_{end_date}.csv"
+    df.to_csv(filepath)
+    print(f"Saved to {filepath}")
+    return df
+
+
+if __name__ == "__main__":
+    df = download_stock_data("AAPL", "2020-01-01", "2024-01-01")
+    print(df.head())
